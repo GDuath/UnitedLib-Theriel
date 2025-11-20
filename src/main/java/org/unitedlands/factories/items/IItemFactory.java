1d9fdf245815d3dbfb5f7f23dcbe9b2b929de97e
GDuath <duath83@gmx.de>
2025-11-20 12:30:20 +0100

    Added UnitedStorage methods to ItemFactory

--- src/main/java/org/unitedlands/factories/items/IItemFactory.java
+++ src/main/java/org/unitedlands/factories/items/IItemFactory.java
@@ -1,9 +1,17 @@
 package org.unitedlands.factories.items;
 
+import java.util.List;
+
+import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 
 public interface IItemFactory {
     boolean isItem(ItemStack item1, ItemStack item2);
     ItemStack getItemStack(String material, int amount);
     ItemStack getItemStack(String material, int minAmount, int maxAmount);
+
+    List<String> getItemList();
+    boolean isValidItem(String itemName);
+    String getFilterName(ItemStack itemStack);
+    boolean isItemInInventory(Inventory inventory, ItemStack item);
 }
