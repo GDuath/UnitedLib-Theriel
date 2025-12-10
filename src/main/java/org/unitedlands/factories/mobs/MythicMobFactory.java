b0a43eae258669970ce1916eed6703835575527b
GDuath <duath83@gmx.de>
2025-12-10 13:30:11 +0100

    Dependency updates. 1.21.10 support

--- src/main/java/org/unitedlands/factories/mobs/MythicMobFactory.java
+++ src/main/java/org/unitedlands/factories/mobs/MythicMobFactory.java
@@ -5,6 +5,7 @@ import java.util.UUID;
 import org.bukkit.Location;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.LivingEntity;
+import org.bukkit.event.entity.CreatureSpawnEvent.SpawnReason;
 import org.unitedlands.utils.Logger;
 
 import io.lumine.mythic.bukkit.MythicBukkit;
@@ -21,12 +22,11 @@ public class MythicMobFactory extends BaseMobFactory {
         var mythicMob = MythicBukkit.inst().getMobManager().getMythicMob(mobType).orElse(null);
         if (mythicMob != null) {
             ActiveMob activeMythicMob = MythicBukkit.inst().getMobManager().spawnMob(mobType, location);
-
             return activeMythicMob.getUniqueId();
         } else {
             try {
                 var entityType = EntityType.valueOf(mobType);
-                var entity = (LivingEntity) location.getWorld().spawnEntity(location, entityType);
+                var entity = (LivingEntity) location.getWorld().spawnEntity(location, entityType, SpawnReason.CUSTOM);
 
                 return entity.getUniqueId();
             } catch (Exception ex) {
